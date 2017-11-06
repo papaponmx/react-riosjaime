a24ad4dd0b4e370c8b36bb6a8e049e15ac1f4089
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-11-05 19:59:07 -0600

    First components.

---
+++ src/components/header-logo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default ({ children }) =>
+  <div style={{ fontWeight: 300, maxWidth: 600 }}>
+    {children}
+  </div>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
+import HeaderLogo from '../components/header-logo';
 import './index.css'
+
+const spacingUnit = `${1.45 * .5}rem`;
 
 const Header = () => (
   <div
     style={{
       background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      marginBottom: spacingUnit,
     }}
   >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: `${spacingUnit} 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -27,38 +29,38 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          <HeaderLogo> Jaime Rios</HeaderLogo>
         </Link>
       </h1>
     </div>
   </div>
-)
+      )
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+      const TemplateWrapper = ({ children }) => (
+        <div>
+          <Helmet
+            title="Gatsby Default Starter"
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          />
+          <Header />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: `0px 1.0875rem ${spacingUnit}`,
+              paddingTop: 0,
+            }}
+          >
+            {children()}
+          </div>
+        </div>
+        )
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+        TemplateWrapper.propTypes = {
+          children: PropTypes.func,
+        }
 
-export default TemplateWrapper
+        export default TemplateWrapper
